ceaf4370b7464a22660254b00f7e135fe2413838
joedoerr <joehdoerr@gmail.com>
2025-07-10 16:11:43 -0400

    prepare inference

--- examples/inference_motoman.py
+++ examples/inference_motoman.py
@@ -1,9 +1,10 @@
 import dataclasses
-
+import time
+import numpy as np
 import jax
 
 from openpi.models import model as _model
-from openpi.policies import droid_policy
+from openpi.policies import moto_policy
 from openpi.policies import policy_config as _policy_config
 from openpi.shared import download
 from openpi.training import config as _config
@@ -16,10 +17,13 @@ checkpoint_dir = download.maybe_download("gs://openpi-assets/checkpoints/motoman
 policy = _policy_config.create_trained_policy(config, checkpoint_dir)
 
 # Run inference on a dummy example. This example corresponds to observations produced by the DROID runtime.
-example = droid_policy.make_motoman_example()
-result = policy.infer(example)
+for i in range(10):
+    example = moto_policy.make_motoman_example() #Will repack then the data transforms will be applied
+    result = policy.infer(example)
+    print("Result:", result, type(result))
+    time.sleep(1)
+
+print(result["actions"].shape)
 
 # Delete the policy to free up memory.
 del policy
-
-print("Actions shape:", result["actions"].shape)
